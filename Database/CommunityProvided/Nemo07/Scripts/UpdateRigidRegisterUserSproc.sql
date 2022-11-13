a25c7631b052395ac3d6e24070f12fe050218dcb
DevTekVE <devtekve@gmail.com>
2022-11-13 11:12:55 +0100

    Updating Nemo's scripts as per his request

--- Database/CommunityProvided/Nemo07/Scripts/UpdateRigidRegisterUserSproc.sql
+++ Database/CommunityProvided/Nemo07/Scripts/UpdateRigidRegisterUserSproc.sql
@@ -1,6 +1,6 @@
 USE [SILKROAD_R_ACCOUNT]
 GO
-/****** Object:  StoredProcedure [dbo].[_Rigid_Register_User]    Script Date: 11/4/2022 10:06:19 PM ******/
+/****** Object:  StoredProcedure [dbo].[_Rigid_Register_User]    Script Date: 11/5/2022 5:35:34 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -13,9 +13,9 @@ ALTER procedure [dbo].[_Rigid_Register_User]
 	@szEmailUniqueStatus	varchar(1),
 	@szCountry	varchar(2),
 	@szRegistrantIP varchar(15),
-	@szVIPUserType int,
-	@szVIPLv int,
-	@szVipExpireTime datetime
+	--@szVIPUserType int,
+	--@szVIPLv int,
+	@szVipExpireTime varchar(20)
 AS
 
 SET NOCOUNT ON
@@ -32,90 +32,97 @@ BEGIN TRANSACTION
 	SET @RegBinIP = [GB_JoymaxPortal].[dbo].[F_MakeIPStringToIPBinary] (@szRegistrantIP)
 
 	IF (EXISTS(SELECT * FROM [GB_JoymaxPortal].[dbo].[MU_User] WHERE UserID = @szUserID))
-BEGIN
-ROLLBACK TRANSACTION
-SELECT -1000 AS 'ReturnCode'
+	BEGIN
+		ROLLBACK TRANSACTION
+		SELECT -1000 AS 'ReturnCode'
 		RETURN -1000
-END
+	END
 
 	IF (EXISTS(SELECT * FROM [GB_JoymaxPortal].[dbo].[MU_Email] WHERE EmailAddr = @szEmail))
-BEGIN
-ROLLBACK TRANSACTION
-SELECT -2000 AS 'ReturnCode'
+	BEGIN
+		ROLLBACK TRANSACTION
+		SELECT -2000 AS 'ReturnCode'
 		RETURN -2000
-END
+	END
 
 	IF (EXISTS(SELECT * FROM [TB_User] WHERE StrUserID = @szUserID))
-BEGIN
-ROLLBACK TRANSACTION
-SELECT -2001 AS 'ReturnCode'
+	BEGIN
+		ROLLBACK TRANSACTION
+		SELECT -2001 AS 'ReturnCode'
 		RETURN -2001
-END
+	END
 
-INSERT INTO [GB_JoymaxPortal].[dbo].[MU_User] (UserID, UserPwd, Gender, Birthday, NickName, CountryCode)
-VALUES(@szUserID, @szPassword, 'M', @RegDate, @szUserID, @szCountry)
-    IF (@@ERROR <> 0 OR @@ROWCOUNT <> 1)
-BEGIN
-ROLLBACK TRANSACTION
-SELECT -3000 AS 'ReturnCode'
+	INSERT INTO [GB_JoymaxPortal].[dbo].[MU_User] (UserID, UserPwd, Gender, Birthday, NickName, CountryCode)
+	VALUES(@szUserID, @szPassword, 'M', @RegDate, @szUserID, @szCountry)
+	IF (@@ERROR <> 0 OR @@ROWCOUNT <> 1)
+	BEGIN
+		ROLLBACK TRANSACTION
+		SELECT -3000 AS 'ReturnCode'
 		RETURN -3000
-END
+	END
 
 	SET @UserJID = @@IDENTITY
 
 	/*TB_User insert move next to MU_User insert*/
+
+		-- VIP 유저 관련
+	-- 
+	-- Based on new/returning/temporary member VIPLevel/grant period
+	SET @szVipExpireTime = Convert(varchar(10),DateADD(day, 8 , getdate()), 121) + ' 00:00:00'
+
+
 	INSERT INTO TB_User (PortalJID, StrUserID, ServiceCompany, [password], Active, UserIP, CountryCode, VisitDate, RegDate, sec_primary, sec_content, sec_grade, AccPlayTime, LatestUpdateTime_ToPlayTime, Email, EmailCertificationStatus, EmailUniqueStatus, VIPUserType, VIPLv, VipExpireTime)
-	VALUES (@UserJID, @szUserID, 11, @szPassword, 1, @szRegistrantIP, @szCountry, GETDATE(), @RegDate, 3, 3, 0, 0, 0, @szEmail, @szEmailCertificationStatus, @szEmailUniqueStatus , @szVIPUserType, @szVIPLv, @szVipExpireTime)
+	VALUES (@UserJID, @szUserID, 11, @szPassword, 1, @szRegistrantIP, @szCountry, GETDATE(), @RegDate, 3, 3, 0, 0, 0, @szEmail, @szEmailCertificationStatus, @szEmailUniqueStatus , 2, 1, @szVipExpireTime)
+	
 	IF (@@ERROR <> 0 OR @@ROWCOUNT <> 1)
-BEGIN
-ROLLBACK TRANSACTION
-SELECT -7000 AS 'ReturnCode'
+	BEGIN
+		ROLLBACK TRANSACTION
+		SELECT -7000 AS 'ReturnCode'
 		RETURN -7000
-END
+	END
 
-INSERT INTO [GB_JoymaxPortal].[dbo].[MUH_AlteredInfo] (JID, AlterationDate, LastName, FirstName, EmailAddr, EmailReceptionStatus, EmailCertificationStatus, UserIP, CountryCode, NickName, ATypeCode, CountryCodeChangingStatus)
-VALUES(@UserJID, @RegDate, @szUserID, @szUserID, @szEmail, 'Y', 'Y', @RegBinIP, @szCountry, @szUserID, 1, 'N')
-    IF (@@ERROR <> 0 OR @@ROWCOUNT <> 1)
-BEGIN
-ROLLBACK TRANSACTION
-SELECT -4000 AS 'ReturnCode'
+	INSERT INTO [GB_JoymaxPortal].[dbo].[MUH_AlteredInfo] (JID, AlterationDate, LastName, FirstName, EmailAddr, EmailReceptionStatus, EmailCertificationStatus, UserIP, CountryCode, NickName, ATypeCode, CountryCodeChangingStatus)
+	VALUES(@UserJID, @RegDate, @szUserID, @szUserID, @szEmail, 'Y', 'Y', @RegBinIP, @szCountry, @szUserID, 1, 'N')
+	IF (@@ERROR <> 0 OR @@ROWCOUNT <> 1)
+	BEGIN
+		ROLLBACK TRANSACTION
+		SELECT -4000 AS 'ReturnCode'
 		RETURN -4000
-END
-
-INSERT INTO [GB_JoymaxPortal].[dbo].[APH_ChangedSilk] (InvoiceID, PTInvoiceID, ManagerGiftID, JID, RemainedSilk, ChangedSilk, SilkType, SellingTypeID, ChangeDate, AvailableDate, AvailableStatus)
-VALUES (NULL, NULL, NULL, @UserJID, 0, 0, 3, 2, @RegDate, DATEADD(YEAR, 10, @RegDate), 'N')
-    IF (@@ERROR <> 0 OR @@ROWCOUNT <> 1)
-BEGIN
-ROLLBACK TRANSACTION
-SELECT -5000 AS 'ReturnCode'
+	END
+	
+	INSERT INTO [GB_JoymaxPortal].[dbo].[APH_ChangedSilk] (InvoiceID, PTInvoiceID, ManagerGiftID, JID, RemainedSilk, ChangedSilk, SilkType, SellingTypeID, ChangeDate, AvailableDate, AvailableStatus)
+	VALUES (NULL, NULL, NULL, @UserJID, 0, 0, 3, 2, @RegDate, DATEADD(YEAR, 10, @RegDate), 'N')
+	IF (@@ERROR <> 0 OR @@ROWCOUNT <> 1)
+	BEGIN
+		ROLLBACK TRANSACTION
+		SELECT -5000 AS 'ReturnCode'
 		RETURN -5000
-END
-
-INSERT INTO [GB_JoymaxPortal].[dbo].[MU_Email] (JID, EmailAddr) VALUES(@UserJID, @szEmail)
-    IF (@@ERROR <> 0 OR @@ROWCOUNT <> 1)
-BEGIN
-ROLLBACK TRANSACTION
-SELECT -6000 AS 'ReturnCode'
+	END
+	
+	INSERT INTO [GB_JoymaxPortal].[dbo].[MU_Email] (JID, EmailAddr) VALUES(@UserJID, @szEmail)
+	IF (@@ERROR <> 0 OR @@ROWCOUNT <> 1)
+	BEGIN
+		ROLLBACK TRANSACTION
+		SELECT -6000 AS 'ReturnCode'
 		RETURN -6000
-END
+	END
 
-INSERT INTO [GB_JoymaxPortal].[dbo].[AUH_AgreedService] ( JID, ServiceCode, StartDate, EndDate, UserIP )
-VALUES ( @UserJID, 11, @RegDate, '9999-12-31 00:00:00', @RegBinIP )
-    IF (@@ERROR <> 0 OR @@ROWCOUNT <> 1)
-BEGIN
-ROLLBACK TRANSACTION
-SELECT -6001 AS 'ReturnCode'
+	INSERT INTO [GB_JoymaxPortal].[dbo].[AUH_AgreedService] ( JID, ServiceCode, StartDate, EndDate, UserIP ) 
+	VALUES ( @UserJID, 11, @RegDate, '9999-12-31 00:00:00', @RegBinIP )
+	IF (@@ERROR <> 0 OR @@ROWCOUNT <> 1)
+	BEGIN
+		ROLLBACK TRANSACTION
+		SELECT -6001 AS 'ReturnCode'
 		RETURN -6001
-END
-
-INSERT INTO [GB_JoymaxPortal].[dbo].[MU_JoiningInfo] ( JID, UserIP, JoiningDate, CountryCode, JoiningPath )
-VALUES ( @UserJID, @RegBinIP, @RegDate, @szCountry, 'JOYMAX' )
-    IF (@@ERROR <> 0 OR @@ROWCOUNT <> 1)
-BEGIN
-ROLLBACK TRANSACTION
-SELECT -6002 AS 'ReturnCode'
+	END
+	
+	INSERT INTO [GB_JoymaxPortal].[dbo].[MU_JoiningInfo] ( JID, UserIP, JoiningDate, CountryCode, JoiningPath )
+	VALUES ( @UserJID, @RegBinIP, @RegDate, @szCountry, 'JOYMAX' )
+	IF (@@ERROR <> 0 OR @@ROWCOUNT <> 1)
+	BEGIN
+		ROLLBACK TRANSACTION
+		SELECT -6002 AS 'ReturnCode'
 		RETURN -6002
-END
+	END
 
 COMMIT TRANSACTION
-
